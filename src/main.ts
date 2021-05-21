b74e654b4df2708954791394c22c863f3635f44a
Emilian Adrian Hossu <emil.hossu@gmail.com>
2021-05-21 13:32:32 +0300

    update and delete DS_Store

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain, dialog } from 'electron';
 import * as path from "path";
+import * as exec from "child_process";
 
+let mainWindow: BrowserWindow;
 
 function createWindow () {
-    const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
       width: 800,
       height: 600,
       webPreferences: {
@@ -11,7 +13,7 @@ function createWindow () {
       }
     })
   
-    win.loadFile(path.join(__dirname, "../index.html"))
+    mainWindow.loadFile(path.join(__dirname, "../index.html"))
   }
   
   app.whenReady().then(() => {
@@ -29,3 +31,59 @@ function createWindow () {
       app.quit()
     }
   })
+
+
+  let fileToConvert: string;
+  let convertedFile: string;
+
+  ipcMain.on('selectFile', (event, args) => {
+   
+    dialog.showOpenDialog(mainWindow, {
+      
+      title: 'Alege fisier',
+      properties: ['openFile']
+    
+    }).then(result => {
+
+
+      if(!result.canceled){
+
+        fileToConvert = result.filePaths[0];
+        
+        const file = path.basename(fileToConvert, 'sav');
+        const dir = path.dirname(fileToConvert);
+        convertedFile = dir + '/' + file + 'dta';
+
+        event.reply('selectFile-reply', { file1: fileToConvert, file2: convertedFile});
+      }
+    
+    }).catch(err => {
+      console.log(err)
+    })
+  });
+
+
+  ipcMain.on('startConvert', (event, args) => {
+
+      // run R command
+      exec.exec('Rscript -e "DDIwR::convert(\''+fileToConvert+'\', to = \''+convertedFile+'\')"', (error, stdout, stderr) => {
+        
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+
+
+        dialog.showMessageBox(mainWindow, {
+          title: 'Merge',
+          message: 'S-a terminat conversia!'
+        })
+
+
+      });
+
+      
+
+  });
--- src/preload.ts
+++ src/preload.ts
@@ -1,10 +1,45 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector: string, text: string) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
+import { ipcRenderer } from "electron";
 
-    for (const type of ["chrome", "node", "electron"]) {
-        replaceText(`${type}-version`, < any > process.versions[type as keyof NodeJS.ProcessVersions]);
-    }
-})
+window.addEventListener('DOMContentLoaded', () => {
+    // const replaceText = (selector: string, text: string) => {
+    //     const element = document.getElementById(selector)
+    //     if (element) element.innerText = text
+    // }
+
+    // for (const type of ["chrome", "node", "electron"]) {
+    //     replaceText(`${type}-version`, <any> process.versions[type as keyof NodeJS.ProcessVersions]);
+    // }
+    
+
+    document.getElementById('selectFile')?.addEventListener('click', function(){
+
+        ipcRenderer.send('selectFile');
+
+        ipcRenderer.on('selectFile-reply', (event, args) => {
+            
+            
+            // const fileName = args.fileName;
+
+            console.log(args.file);
+            
+            const fileToConvert = <HTMLInputElement>document.getElementById('fileToConvert');
+            const convertedFile = <HTMLInputElement>document.getElementById('convertedFile');
+            
+            fileToConvert.value = args.file1;
+            convertedFile.value = args.file2;
+
+        });
+
+    });
+
+    document.getElementById('runConvert')?.addEventListener('click', function(){
+        ipcRenderer.send('startConvert');
+
+
+        // ipcRenderer.on('selectFile-reply', (event, args) => {
+            
+        // })
+
+    });
+
+}) 
